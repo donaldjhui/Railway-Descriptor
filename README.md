05d54f52e7c3c34d2f731139ff57caf7628e2e28
Donald Hui <djhui@ucsc.edu>
2021-03-12 19:55:31 -0800

    Added README

---
+++ README.md
@@ -0,0 +1,1 @@
+Railway descriptor to get distances of routes, shortest routes, and number of different routes from C to C.
--- railway.js
+++ railway.js
@@ -1,25 +1,25 @@
-// Parse inputs for railway paths/nodes and distances
+// Parse inputs for railway routes/nodes and distances
 function createDGraph() {
 
-    // Regex to separate paths/nodes from their distances using a global modifier
+    // Regex to separate routes/nodes from their distances using a global modifier
     var regex = new RegExp('([0-9]+)|([a-zA-Z]+)','g');
 
     var nodes_list = [];
     let nodes_map = {};
 
-    // Request graph nodes and organize the paths/nodes into an array
+    // Request graph routes/nodes and organize the routes/nodes into an array
     read.question('Input Nodes of the Directed Graph, separated by commas: ', (nodes) => {
         
         nodes_list = nodes.split(',');
         nodes_list = nodes_list.map(node => node.trim());
 
         for (var i = 0; i < nodes_list.length; i++) {
-            // Set of paths/nodes
+            // Set of routes/nodes
             node_set = nodes_list[i].match(regex)[0];
-            // The distance between the set of paths/nodes
+            // The distance between the set of routes/nodes
             node_distance = nodes_list[i].match(regex)[1];
 
-            // Add the paths/nodes and distances into a map
+            // Add the routes/nodes and distances into a map
             nodes_map[node_set] = node_distance;
         }
 
@@ -27,7 +27,7 @@ function createDGraph() {
     });
 }
 
-// Get distances of railway paths/nodes
+// Get distances of railway routes/nodes
 function findDistances(nodes) {
     console.log(nodes);
     read.close();
